5a9291c753a0db1980038725efa96eff8c666f2b
Hearty Oh <heartyoh@hatiolab.com>
2019-04-18 17:44:52 +0800

    v0.0.2

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,25 +1,25 @@
 import { store, showSnackbar, i18next } from '@things-factory/shell'
 
-function onProfileChanged(e) {}
-
-function onAuthenticatedChanged(e) {
-  var auth = e.detail
-  store.dispatch(
-    showSnackbar(
-      i18next.t('text.you.are.now.in', {
-        state: {
-          text: i18next.t(auth.authenticated ? 'text.signed in' : 'text.signed out')
-        }
-      })
-    )
-  )
-}
-
-function onAuthErrorChanged(e) {
-  store.dispatch(showSnackbar(e.detail))
-}
-
 export default function bootstrap() {
+  function onProfileChanged(e) {}
+
+  function onAuthenticatedChanged(e) {
+    var auth = e.detail
+    store.dispatch(
+      showSnackbar(
+        i18next.t('text.you.are.now.in', {
+          state: {
+            text: i18next.t(auth.authenticated ? 'text.signed in' : 'text.signed out')
+          }
+        })
+      )
+    )
+  }
+
+  function onAuthErrorChanged(e) {
+    store.dispatch(showSnackbar(e.detail))
+  }
+
   document.addEventListener('profile-changed', onProfileChanged)
   document.addEventListener('authenticated-changed', onAuthenticatedChanged)
   document.addEventListener('auth-error-changed', onAuthErrorChanged)
