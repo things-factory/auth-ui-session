a265dde3d6bb43aae5c55e6e138c87d15d5c3562
Hearty Oh <heartyoh@hatiolab.com>
2019-05-16 12:02:02 +0900

    v0.1.21

--- client/route.js
+++ client/route.js
@@ -1,15 +1,15 @@
 export default function route(page) {
   switch (page) {
     case 'signup':
-      import('./pages/signup.js.js')
+      import('./pages/signup.js')
       return page
 
     case 'signin':
-      import('./pages/signin.js.js')
+      import('./pages/signin.js')
       return page
 
     case 'profile':
-      import('./pages/profile.js.js')
+      import('./pages/profile.js')
       return page
   }
 }
