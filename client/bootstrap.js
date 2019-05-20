8b96a94a4bf6bbd6d7607f4459ca5c381f0a694a
Hearty Oh <heartyoh@hatiolab.com>
2019-05-20 15:50:00 +0900

    v0.1.22

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -2,7 +2,7 @@ import { html } from 'lit-html'
 import { store, loadPage } from '@things-factory/shell'
 import { auth } from '@things-factory/auth-base'
 import { i18next } from '@things-factory/i18n-base'
-import { showSnackbar, APPEND_APP_TOOL, TOOL_POSITION } from '@things-factory/layout-base'
+import { APPEND_APP_TOOL, TOOL_POSITION } from '@things-factory/layout-base'
 import { ADD_MORENDA } from '@things-factory/more-base'
 import { ADD_SETTING } from '@things-factory/setting-base'
 
@@ -25,19 +25,30 @@ export default function bootstrap() {
   }
 
   function onAuthentication(on) {
-    store.dispatch(
-      showSnackbar(
-        i18next.t('text.you.are.now.in', {
-          state: {
-            text: i18next.t(on ? 'text.signed in' : 'text.signed out')
-          }
-        })
-      )
+    document.dispatchEvent(
+      new CustomEvent('notify', {
+        detail: {
+          type: 'info',
+          message: i18next.t('text.you.are.now.in', {
+            state: {
+              text: i18next.t(on ? 'text.signed in' : 'text.signed out')
+            }
+          })
+        }
+      })
     )
   }
 
   function onError(e) {
-    store.dispatch(showSnackbar(e))
+    document.dispatchEvent(
+      new CustomEvent('notify', {
+        detail: {
+          type: 'error',
+          message: e,
+          e
+        }
+      })
+    )
   }
 
   auth.on('profile', onProfile)
